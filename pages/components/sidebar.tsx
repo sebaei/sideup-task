8c585d7ef8163cdcf1d5e30fafb4697eaa6b7793
sebaei <47517850+sebaei@users.noreply.github.com>
2023-01-07 14:35:32 +0200

    Adding assets and Sidebar

---
+++ pages/components/sidebar.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import sideupLogo from "../../public/assets/sideupLogo.jpg";
+import disk from "../../public/assets/sideupLogo.jpg";
+
+import { sidebarItems } from "../../public/assets/constants";
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <img
+          src={sideupLogo}
+          alt="logo"
+          className="w-14 h-8 object-contain bg-white"
+        />
+        {sidebarItems.map((item, index) => (
+          <div key={index} className="flex flex-row">
+            <img src={disk} className="w-4 h-4" />
+            <span className="ml-2">{item}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ public/assets/constants.ts
@@ -0,0 +1,13 @@
+export const sidebarItems = [
+  "Dashboard",
+  "My Shipments",
+  "My Customers",
+  "Reports",
+  "Cash Transfers",
+  "Invoices",
+  "Integrations",
+  "Marketing Tools",
+  "International Princess",
+  "Support",
+  "Sign Out",
+];
